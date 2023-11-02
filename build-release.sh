ffcf4310256cfd495d8003c78d0cd49a919e1aa1
ibuetler <ivan.buetler@hacking-lab.com>
2023-11-02 22:37:41 +0100

    fixing

--- build-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build --no-cache -t hackinglab/alpine-nginx-nodejs-websocketd-hl:$1.0 -t hackinglab/alpine-nginx-nodejs-websocketd-hl:$1 -t hackinglab/alpine-nginx-nodejs-websocketd-hl:latest -f Dockerfile .
-
-docker push hackinglab/alpine-nginx-nodejs-websocketd-hl
-docker push hackinglab/alpine-nginx-nodejs-websocketd-hl:$1
-docker push hackinglab/alpine-nginx-nodejs-websocketd-hl:$1.0
-
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t hackinglab/alpine-nginx-nodejs-websocketd-hl:3.2.0 -t hackinglab/alpine-nginx-nodejs-websocketd-hl:3.2 -t hackinglab/alpine-nginx-nodejs-websocketd-hl:latest -f Dockerfile .
-
